04d22a6786a6f58c195012d14b1c11b703cd9250
omsah <huncyoms@gmail.com>
2023-05-16 13:07:43 -0400

    dead code

--- Condtitions5.java
+++ Condtitions5.java
@@ -5,6 +5,7 @@ public class Condtitions5 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
+        sc.close();
 
         if (a == b) {
             System.out.printf("%d is equal with %d", a, b);
@@ -12,7 +13,11 @@ public class Condtitions5 {
             System.out.printf("%d is greater than %d.", a, b);
         }
         else {
-            System.out.printf("%d is less than %d",a,b);
+            System.out.printf("%d is less than %d\n", a, b);
         }
+        
+
+        if (3 % 2 == 0);
+        System.out.println("Dead code.");
     }
 }
