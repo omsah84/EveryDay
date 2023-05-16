2f369bbdbac4d07bc1335c6c6206455d72671348
omsah <huncyoms@gmail.com>
2023-05-16 12:41:37 -0400

    input

---
+++ input3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//input
+public class input3 {
+    public static void main(String[] ar) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a+b;
+
+        System.out.print("The sum of two is: "+ sum);
+    }
+
+}
