867b3fa29ea037a4c9d37565f1380f970c5b05c6
omsah <huncyoms@gmail.com>
2023-05-16 12:59:31 -0400

    greater number

---
+++ Condtitions5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Condtitions5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == b) {
+            System.out.printf("%d is equal with %d", a, b);
+        } else if (a > b) {
+            System.out.printf("%d is greater than %d.", a, b);
+        }
+        else {
+            System.out.printf("%d is less than %d",a,b);
+        }
+    }
+}
