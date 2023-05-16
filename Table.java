cda17bffc5f9a83f8d50a1420de7a396a397b70d
omsah <huncyoms@gmail.com>
2023-05-16 14:32:43 -0400

    multiplication table using recursion

---
+++ Table.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void printTable(int n, int idx) {
+
+        if (idx == 10) {
+            System.out.printf("%d * %d = %d", n ,idx , (n * idx));
+            return;
+        }
+        
+        System.out.printf("%d * %d = %d\n", n, idx, (n * idx));
+        printTable(n, idx+1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        sc.close();
+
+        printTable(n,1);
+    }
+}
