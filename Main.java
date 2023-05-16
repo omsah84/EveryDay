e05da19ae1b4dea385bcdfb3effc33a5cc8daf78
omsah <huncyoms@gmail.com>
2023-05-16 12:21:25 -0400

    first program in java

---
+++ Main.java
@@ -0,0 +1,9 @@
+//first program in java
+
+public class Main {
+    public static void main(String[] ar) {
+        System.out.print("Hello, World\n");
+        System.out.print("Hello, second Line\t");
+        System.out.println("Hello, Third LIne.");
+    }
+}
