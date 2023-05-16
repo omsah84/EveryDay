2d82c4c2ead3f478600eede4a5fcb18744e85d98
omsah <huncyoms@gmail.com>
2023-05-16 12:26:33 -0400

    basic pattern

--- Main0.java
+++ Main0.java
@@ -1,6 +1,6 @@
 //first program in java
 
-public class Main {
+public class Main0 {
     public static void main(String[] ar) {
         System.out.print("Hello, World\n");
         System.out.print("Hello, second Line\t");
---
+++ Pattern1.java
@@ -0,0 +1,11 @@
+//basic pattern 
+public class Pattern1 {
+
+    
+    public static void main(String[] ar) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.print("****");
+    }
+}
