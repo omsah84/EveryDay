eae4b704eec2c623098edb6bfdf42ee04147b4f6
omsah <huncyoms@gmail.com>
2023-05-16 12:37:54 -0400

    variable

--- Pattern1.java
+++ Pattern1.java
@@ -1,7 +1,5 @@
 //basic pattern 
 public class Pattern1 {
-
-    
     public static void main(String[] ar) {
         System.out.println("*");
         System.out.println("**");
---
+++ Var2.java
@@ -0,0 +1,9 @@
+public class Var2 {
+    public static void main(String[] arr) {
+        //variable
+        int a = 30;
+        int b = 34;
+        int sum = a + b;
+        System.out.printf("The addition of %d and %d is: " + a, b, sum);
+    }
+}
