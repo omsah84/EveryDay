04286fc655311b52de9eb2ab18edb3c264fcee9f
omsah <huncyoms@gmail.com>
2023-05-18 11:38:56 -0400

    commit

--- Pro2.java
+++ Pro2.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner;
+
 public class Pro2 {
+
+    public static int AddTwoNumber(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] ar) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = AddTwoNumber(a, b);
+        System.out.println(sum);
+
+    }
     
 }
