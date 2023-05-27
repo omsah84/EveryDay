a684996c09ea8bf52410f1d773eef67d69d9a75e
omsah <huncyoms@gmail.com>
2023-05-27 00:45:54 -0400

    cpp

--- SimpleIn.cpp
+++ SimpleIn.cpp
@@ -1,5 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    #
+int main()
+{
+#ifndef ONLINE_JUDEGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int p, t, r;
+    cin >> p >> t >> r;
+    cout << "\nThe simple intrest is: ";
+    cout << (p * t * r) / 100;
+
+    return 0;
 }
