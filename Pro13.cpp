63bd0bdf051c63154ece0d8ac09d5a02368a6c96
omsah <huncyoms@gmail.com>
2023-06-01 00:15:40 -0400

    commit

---
+++ Pro13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+bool isLeapYear(int year) {
+    if (year % 4 != 0) {
+        return false;
+    } else if (year % 100 != 0) {
+        return true;
+    } else if (year % 400 != 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+int main() {
+    int year;
+
+    std::cout << "Enter a year: ";
+    std::cin >> year;
+
+    if (isLeapYear(year)) {
+        std::cout << year << " is a leap year." << std::endl;
+    } else {
+        std::cout << year << " is not a leap year." << std::endl;
+    }
+
+    return 0;
+}
