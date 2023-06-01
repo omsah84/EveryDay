c1b3398fee46a28f4729cfe277b1dc7c0fa4fe3e
omsah <huncyoms@gmail.com>
2023-05-31 23:16:16 -0400

    commit

---
+++ pro1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    std::cout << "You entered: " << number << std::endl;
+
+    return 0;
+}
