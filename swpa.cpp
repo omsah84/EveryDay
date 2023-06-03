864c5a6a37abc4cba06c5d49ad48c0aabefad22c
omsah <huncyoms@gmail.com>
2023-06-03 11:42:42 -0400

    commit

---
+++ swpa.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void swape(int arr[], int size){
+    for (int i = 0 i < size;i++){
+        if((i+1)<size){
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+}
+
+int main(){
+    
+}
