1cb14afe884dbacf57b8b13630f9d9fdf893ebae
omsah <huncyoms@gmail.com>
2023-06-13 13:06:58 -0400

    commit

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -1,8 +1,49 @@
-public class BinaryToDecimal{
-    public static void main(String[] arr){
-        String binarynum = "101010";
+// public class BinaryToDecimal{
+//     public static void main(String[] arr){
+//         String binarynum = "101010";
 
-        int decimalNum = binarytodecimal(binarynum);
+//         int decimalNum = binarytodecimal(binarynum);
+//         System.out.println("Decimal: "+decimalNum);
+//     }
+
+//     public static int binarytodecimal(String binary){
+//         int decimal = 0;
+//         int length = binary.length() -1;
+
+//         for(int i = length ;i>=0;i--){
+
+            
+//             char digit = binary.charAt(i);
+//             if(digit == '1'){
+//                 decimal = decimal + Math.pow(2,length-i);
+//             }
+//         }
+
+//         return decimal;
         
+//     }
+// }
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "101011"; // Replace with your binary number
+
+        int decimal = binaryToDecimal(binary);
+        System.out.println("Decimal: " + decimal);
+    }
+
+    public static int binaryToDecimal(String binary) {
+        int decimal = 0;
+        int length = binary.length();
+        
+        for (int i = length - 1; i >= 0; i--) {
+            char digit = binary.charAt(i);
+            
+            if (digit == '1') {
+                decimal += Math.pow(2, length - i - 1);
+            }
+        }
+        
+        return decimal;
     }
 }
---
+++ convet.java
@@ -0,0 +1,9 @@
+public class convet{
+    public static void main(String[] arr){
+        char cr = '1';
+
+        int chr = (int)cr;
+
+        System.out.println("The character is: " +chr);
+    }
+}
