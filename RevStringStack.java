92f4bdfea9223c656275dbbdd680ac3ca658397a
omsah <huncyoms@gmail.com>
2023-06-15 06:23:46 -0400

    commit

---
+++ RevStringStack.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class RevStringStack {
+    public static void main(String[] args) {
+
+        Stack<Character> st = new Stack<>();
+        String str = "omsah";
+        
+
+        for(int i =0;i<str.length();i++){
+            char neewStr = str.charAt(i);
+
+            st.push(neewStr);
+        }
+
+        
+        
+        
+
+
+        
+    }
+}
